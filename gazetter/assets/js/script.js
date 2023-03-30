a6c57890f73849a3acdb1ba889793cd87794ada6
KERRIE MCIVOR <kerriemcivor92@gmail.com>
2023-03-30 13:37:10 +0100

    adding loader html and functionality

--- gazetter/assets/js/script.js
+++ gazetter/assets/js/script.js
@@ -1,12 +1,24 @@
+document.getElementById('map').onload = function () {
+    document.getElementById('map').style.display = "block";
+    document.getElementById('loader').style.display = "none";
+}
+
+document.getElementById('map').onerror = function () {
+    console.log("Failed to load map");
+    document.getElementById('loader').style.display = "none";
+    // Add code to display an error message or retry loading the map
+}
+
+
 //using php array to add options to select box
-$.ajax({
+/*$.ajax({
     url: "assets/php/php.php",
     type: 'POST',
     dataType: 'json',
     data: {
-        countryName: 'hello',
+        countryName: '',
     },
     success: function(result) {
         console.log(result)
     }
-});
+});*/
